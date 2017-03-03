c2a328777130e959bde02943ca19d76cf2610553
Alessio <antonini@di.unito.it>
2017-03-03 15:39:54 +0100

    aggiunta scrittura mbtiles

---
+++ server.js
@@ -0,0 +1,4 @@
+/**
+ * Created by drpollo on 03/03/2017.
+ */
+//test tessera mbtiles://./output.mbtiles
--- test.js
+++ test.js
@@ -1,8 +1,16 @@
+var tilelive = require('@mapbox/tilelive');
 var Omnivore = require('@mapbox/tilelive-omnivore');
+var mbtile = require('mbtiles');
 var filepath = __dirname+'/geojson/level_6.geojson';
 var uri = 'omnivore://' + filepath;
+var uriMB = __dirname+'/output.mbtiles'
 
 new Omnivore(uri, function(err, source) {
+  var readable = tilelive.createReadStream(source,{type:'pyramid',job:{total:4,num:1}, maxzoom:8, minzoom:8});
+  new mbtile(uriMB, function(err,sink){
+    var writable = tilelive.createWriteStream(sink);
+    readable.pipe(writable);
+  })
   source.getInfo(function(err, info) {
     console.log(info);
   });
