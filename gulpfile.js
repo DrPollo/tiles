3cbb7e206a7d6522432d35fb8b0ee63bb31d5fc1
Stefania <buccolie@di.unito.it>
2017-06-23 17:40:38 +0200

    aggiunta api di get Area from point : z/lon/lat

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var UUID = require('uuid');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var concat = require('gulp-concat');
@@ -12,6 +13,7 @@ var jsonMinify = require('gulp-jsonminify');
 
 var geojsonpath = './geojson/';
 var tippecanoepath = 'tippecanoe/';
+var tippecanoepathLabels = 'tippecanoepathLabels/'
 var minpath = 'min/';
 var sourcemap = 'sourcemap.json';
 var admin_level_map = 'admin_level_map.json';
@@ -35,6 +37,17 @@ gulp.task('load_static_geojson',function () {
         });
     }
 
+    // carico il file di admin_level_map
+    try {
+        var admin_map = fse.readJsonSync(geojsonpath+admin_level_map);
+    } catch (err) {
+        console.error('directory read error ', err);
+        throw new gutil.PluginError({
+            plugin: 'readFileSync',
+            message: geojsonpath+admin_level_map+" read error"
+        });
+    }
+
     // carico i file geosjon "statici"
     try {
         var static_dir = geojsonpath + "/static/"
@@ -50,6 +63,7 @@ gulp.task('load_static_geojson',function () {
     console.log('files to read: ',static_files.length);
     // creo le directory per i file
     fse.ensureDirSync(geojsonpath+tippecanoepath);
+    fse.ensureDirSync(geojsonpath+tippecanoepathLabels);
 
     // ciclo i file in static
     for(var i in static_files) {
@@ -68,7 +82,23 @@ gulp.task('load_static_geojson',function () {
                 }
 
                 if(ok){
+                    var zoom_min = 22;
+                    var zoom_max = 0;
+                    for(var a in admin_map){
+                        if(admin_map[a].files.includes(file)){
+                            if(admin_map[a].minzoom < zoom_min) zoom_min = admin_map[a].minzoom;
+                            if(admin_map[a].maxzoom > zoom_max) zoom_max = admin_map[a].maxzoom;
+                        }
+                    }
+                    console.log(file,zoom_min,zoom_max)
                     var newFeatures = features.map(function(feature){
+
+                        feature.properties.id = UUID.v1();
+                        feature.properties.type = sources[file].layer;
+                        feature.properties.zoom_min = zoom_min;
+                        feature.properties.zoom_max = zoom_max;
+                        feature.properties.z_index = sources[file].z_index;
+
                         feature['tippecanoe'] = {
                             "maxzoom" : sources[file].maxzoom,
                             "minzoom" : sources[file].minzoom,
@@ -77,8 +107,6 @@ gulp.task('load_static_geojson',function () {
                         try {
                             delete feature.properties.bbox;
                             delete feature.properties.geometry;
-                            delete feature.properties.zoom_min;
-                            delete feature.properties.zoom_max;
                         }catch (err){
 
                         }
@@ -102,6 +130,8 @@ gulp.task('load_static_geojson',function () {
                     var newFeaturesLabels = features.map(function(feature){
 
                         var center = polygonCenter(feature.geometry);
+                        feature.properties.id = UUID.v1();
+                        feature.properties.type = sources[file].layer;
 
                         feature.geometry = center;
 
@@ -124,15 +154,15 @@ gulp.task('load_static_geojson',function () {
 
                     try {
                         // cancello il file _labels
-                        fs.unlinkSync(geojsonpath+tippecanoepath+"labels_"+file);
+                        fs.unlinkSync(geojsonpath+tippecanoepathLabels+"labels_"+file);
                     }catch (err){
                         console.log('nothing to delete');
                     }
                     try{
                         // scrivo il file _labels
-                        fse.writeJsonSync(geojsonpath+tippecanoepath+"labels_"+file,newFeaturesLabels);
+                        fse.writeJsonSync(geojsonpath+tippecanoepathLabels+"labels_"+file,newFeaturesLabels);
                     }catch (err){
-                        console.error('ERROR: cannot generate file ',file, " in ", geojsonpath+tippecanoepath);
+                        console.error('ERROR: cannot generate file ',file, " in ", geojsonpath+tippecanoepathLabels);
                     }
 
                 }
@@ -156,6 +186,17 @@ gulp.task('load_osm_geojson',function () {
         });
     }
 
+    // carico il file di admin_level_map
+    try {
+        var admin_map = fse.readJsonSync(geojsonpath+admin_level_map);
+    } catch (err) {
+        console.error('directory read error ', err);
+        throw new gutil.PluginError({
+            plugin: 'readFileSync',
+            message: geojsonpath+sourcemap+" read error"
+        });
+    }
+
     // carico i file geosjon "osm"
     try {
         var osm_dir = geojsonpath + "/turin_italy_osm_geojson/"
@@ -171,8 +212,9 @@ gulp.task('load_osm_geojson',function () {
     console.log('files to read: ',osm_files.length);
     // creo le directory per i file
     fse.ensureDirSync(geojsonpath+tippecanoepath);
+    fse.ensureDirSync(geojsonpath+tippecanoepathLabels);
 
-    // ciclo i file in static
+    // ciclo i file 
     for(var i in osm_files) {
         var file = osm_files[i];
         if(file.search('.geojson') !== -1 ) {
@@ -189,7 +231,21 @@ gulp.task('load_osm_geojson',function () {
                 }
 
                 if(ok){
+                    var zoom_min = 22;
+                    var zoom_max = 0;
+                    for(var a in admin_map){
+                        if(admin_map[a].files.includes(file)){
+                            if(admin_map[a].minzoom < zoom_min) zoom_min = admin_map[a].minzoom;
+                            if(admin_map[a].maxzoom > zoom_max) zoom_max = admin_map[a].maxzoom;
+                        }
+                    }
                     var newFeatures = features.map(function(feature){
+                        feature.properties.id = UUID.v1();
+                        feature.properties.type = sources[file].layer;
+                        feature.properties.zoom_min = zoom_min;
+                        feature.properties.zoom_max = zoom_max;
+                        feature.properties.z_index = sources[file].z_index;
+
                         feature['tippecanoe'] = {
                             "maxzoom" : sources[file].maxzoom,
                             "minzoom" : sources[file].minzoom,
@@ -198,8 +254,6 @@ gulp.task('load_osm_geojson',function () {
                         try {
                             delete feature.properties.bbox;
                             delete feature.properties.geometry;
-                            delete feature.properties.zoom_min;
-                            delete feature.properties.zoom_max;
                         }catch (err){
 
                         }
@@ -223,6 +277,9 @@ gulp.task('load_osm_geojson',function () {
 
                     var newFeaturesLabels = features.map(function(feature){
 
+                        feature.properties.id = UUID.v1();
+                        feature.properties.type = sources[file].layer;
+                        
                         var geoType = feature.geometry.type;
                         var geom=feature.geometry;
                         var area = 0;
@@ -268,15 +325,15 @@ gulp.task('load_osm_geojson',function () {
                     });
                     try {
                         // cancello il file _labels
-                        fs.unlinkSync(geojsonpath+tippecanoepath+"labels_"+file);
+                        fs.unlinkSync(geojsonpath+tippecanoepathLabels+"labels_"+file);
                     }catch (err){
                         console.log('nothing to delete');
                     }
                     try{
                         // scrivo il file _labels
-                        fse.writeJsonSync(geojsonpath+tippecanoepath+"labels_"+file,newFeaturesLabels);
+                        fse.writeJsonSync(geojsonpath+tippecanoepathLabels+"labels_"+file,newFeaturesLabels);
                     }catch (err){
-                        console.error('ERROR: cannot generate file ',file, " in ", geojsonpath+tippecanoepath);
+                        console.error('ERROR: cannot generate file ',file, " in ", geojsonpath+tippecanoepathLabels);
                     }
                 }
             }
@@ -332,7 +389,7 @@ gulp.task('generatembtile',function() {
 
         var mbtilesfilename = i;
 
-        var cmd = 'tippecanoe --output ' +mbtilespath+mbtilesfilename+ ' --force -pf -pk --minimum-zoom=' + admin_map[i].minzoom + ' --maximum-zoom=' + admin_map[i].maxzoom + ' --preserve-input-order ';
+        var cmd = 'tippecanoe --output ' +mbtilespath+mbtilesfilename+ ' --force -pf -pk --minimum-zoom=' + admin_map[i].minzoom + ' --maximum-zoom=' + admin_map[i].maxzoom;
 
         for(var f in admin_map[i].files){
             console.log('reading file ', admin_map[i].files[f]);
