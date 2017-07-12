8ca23f302da76d3981f71f816470ad391669071d
Alessio <antonini@di.unito.it>
2017-07-12 11:32:19 +0200

    fix cors

--- server.js
+++ server.js
@@ -33,6 +33,11 @@ fs.readFile(filepath, 'utf8', function (err, data) {
     obj = JSON.parse(data);
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/tile/:z/:x/:y', function(req, res) {
 
