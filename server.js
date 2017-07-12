853db450fc0bfb3ecd0eec6705cf63fbbaba35f3
Alessio <antonini@di.unito.it>
2017-07-12 11:51:26 +0200

    config cors

--- server.js
+++ server.js
@@ -4,7 +4,12 @@
 var express = require('express');
 var cors = require('cors')
 var app = express();
-app.use(cors());
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}));
 
 
 var fs = require('fs');
