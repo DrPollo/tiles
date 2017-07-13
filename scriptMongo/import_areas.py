11d8fa84334aaa551bbc979dc555c4774a60417f
Stefania <buccolie@di.unito.it>
2017-07-13 11:09:42 +0200

    aggiunta cartella scriptMongo per aggiunta aree

---
+++ scriptMongo/import_areas.py
@@ -0,0 +1,82 @@
+import json
+import pymongo
+from pymongo import MongoClient
+from bson import ObjectId
+import os
+
+
+client = MongoClient()
+
+db = client['fl_V4']
+
+def create_new_collection(collection_name):
+
+    if(collection_name in db.collection_names()):
+        db[collection_name].drop()
+    
+    collection = db.create_collection(collection_name)
+
+def import_new_collection(file_name,collection_name):
+
+    with open(file_name) as data_file:
+        from_file = json.load(data_file)
+
+    if(collection_name in db.collection_names()):
+        db[collection_name].drop()
+
+    if(len(from_file) > 0):
+        collection = db.create_collection(collection_name)
+        for id_a in from_file:
+		uuid = id_a["properties"]["id"]
+		id_a["_id"] = uuid
+		#print("import",uuid)
+		collection.insert(id_a)
+
+def append_new_collection(file_name,collection_name):
+
+    with open(file_name) as data_file:
+        from_file = json.load(data_file)
+
+    if(collection_name in db.collection_names()):
+        if(len(from_file) > 0):
+        	for id_a in from_file:
+			uuid = id_a["properties"]["id"]
+			id_a["_id"] = uuid
+			#print("append",uuid)
+	    		db[collection_name].insert(id_a)
+    else:
+        import_new_collection(file_name,collection_name)
+
+def set_ID():
+
+    allRowAreas = db.allRowAreas.find()
+    create_new_collection('AllAreas')
+    
+    for id_a in allRowAreas:
+    	uuid = id_a["properties"]["id"]
+    	id_a["_id"] = uuid
+    	
+    	db.AllAreas.insert(id_a)
+    	
+    db.AllAreas.update({}, { "$unset": {"properties.id": 1, "tippecanoe": 1} }, upsert=False, multi=True)
+
+def main():
+	path = os.getcwd() 
+	files = os.listdir(path)
+	allRowAreas = [i for i in files if i.endswith('.geojson')]
+	#print(allRowAreas)
+	
+	db['allRowAreas'].drop()
+
+	for area in allRowAreas:
+		print(area)
+		append_new_collection(area,'allRowAreas')
+		
+	set_ID()
+	
+	#mongoexport --db fl_V4 --collection AllAreas --out AllAreas.json
+	
+
+
+if __name__ == "__main__":
+    main()
